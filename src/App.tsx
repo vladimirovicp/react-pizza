3a166a05b4fde89f70ef0df23382361bd6875c64
vladimirovicp <vladimirovicp70@gmail.com>
2024-07-01 00:20:19 +0400

    83. Создание роутов

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import Button from './compotents/Button/Button';
 import Input from './compotents/Input/input';
+import { Route, Routes } from 'react-router-dom';
+import { Cart } from './pages/Cart/Cart';
+import { Menu } from './pages/Menu/Menu';
+import { Error } from './pages/Error/Error';
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -13,6 +17,16 @@ function App() {
       }>Кнопка</Button>  
       <Button appearance='big'>Кнопка 2</Button>
       <Input placeholder='Email'/>
+      <div>
+        <a href='/'>Меню</a>
+        <a href='/cart'>Корзина</a>
+      </div>
+
+      <Routes>
+        <Route path='/' element={<Menu />}/>
+        <Route path='/cart' element={<Cart />}/>
+        <Route path='*' element={<Error />}/>
+      </Routes>
     </>
   );
 }
